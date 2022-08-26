4a810ffab7e4e8c1add5f46dca6d20fb48fabc24
Cfmunoz8 <cfmunoz8@gmail.com>
2022-08-26 23:04:59 +0000

    actualización myDomain

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ let pronoun = ['the','our', 'my'];
 let adj = ['great', 'big', 'little' ];
 let noun = ['jogger','racoon', 'mouse'];
 let domain = ['.com', '.net', '.cl']
+let myDomain = []
 
 for (let i=0; i < pronoun.length; i++){
     for (let j=0; j < adj.length; j++){
         for(let k=0; k< noun.length; k++){
             for (let l=0; l<domain.length; l++){
-                console.log(pronoun[i] + adj[j] + noun[k] + domain[l])
+                myDomain.push(pronoun[i] + adj[j] + noun[k] + domain[l])
             }
         }
     }    
 }
+console.log (myDomain)
